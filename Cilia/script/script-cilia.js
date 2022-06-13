9276094566d106044d57a67ac5cc54ed274b78e3
Cilia Poellhuber <cilia,pollhuber@gmail.com>
2022-06-13 10:26:18 +0200

    crystals

--- Cilia/script/script-cilia.js
+++ Cilia/script/script-cilia.js
@@ -1,13 +1,13 @@
 
 
-let aboutUsText = ["This is the Smoky Quartz and you can find some infos about it here. When you scroll down you can find some cool outfits."]
+let aboutUsText = ["This is the Smoky Quartz and you can find some infos about it here. When you scroll down you can find some cool outfits;)"]
 function openForm2() {
 
     document.getElementById("myForm2").style.display = "block";
     document.getElementById("facts2").innerHTML = aboutUsText[0];
 }
 
-let aboutUsText1 = ["This is the Ruby and you can find some infos about it here. When you scroll down you can find some cool outfits."]
+let aboutUsText1 = ["This is the Ruby and you can find some infos about it here. When you scroll down you can find some cool outfits;)"]
 
 function openForm1(){
     document.getElementById("myForm2").style.display = "block";
--- Deborah/script/script-deborah.js
+++ Deborah/script/script-deborah.js
@@ -1,13 +1,13 @@
 
 
-let aboutUsText = ["This is the Aegirine and you can find some infos about it here. When you scroll down you can find some cool outfits."]
+let aboutUsText = ["This is the Aegirine and you can find some infos about it here. When you scroll down you can find some cool outfits;)"]
 function openForm2() {
 
     document.getElementById("myForm2").style.display = "block";
     document.getElementById("facts2").innerHTML = aboutUsText[0];
 }
 
-let aboutUsText1 = ["This is the Agate Crazy Lace and you can find some infos about it here. When you scroll down you can find some cool outfits."]
+let aboutUsText1 = ["This is the Agate Blue Banded and you can find some infos about it here. When you scroll down you can find some cool outfits;)"]
 
 function openForm1(){
     document.getElementById("myForm2").style.display = "block";
